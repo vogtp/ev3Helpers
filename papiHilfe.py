f15cf67f8d66325f10539074ebee54c094ecd8fc
Patrick Vogt <patrick.vogt@unibas.ch>
2019-12-30 18:37:04 +0100

    iterate images and sounds

--- papiHilfe.py
+++ papiHilfe.py
@@ -51,3 +51,67 @@ def warten_am_anfang():
     brick.light(None)
     # Bildschirm löschen
     brick.display.clear()   
+
+def bilder_zeigen():
+    '''
+    Zeigt alle Bilder
+    '''
+    # Ist zu kompliziert im Moment
+    import inspect 
+    members = inspect.getmembers(ImageFile)
+    members = [(name, data) for (name, data) in members if not name.startswith('_')]
+    i=0
+    while True:
+        name, data = members[i]
+        print('ImageFile.{}'.format(name))
+        brick.display.clear()
+        brick.display.image(data)
+        brick.display.text("")
+        brick.display.text('ImageFile.{}'.format(name))
+        wait(50)
+        buttons = brick.buttons()
+        while not any(buttons):
+            buttons = brick.buttons()
+            wait(10)
+        if Button.RIGHT in buttons:
+            i += 1
+        if Button.LEFT in buttons: 
+            i -= 1
+        if Button.CENTER in buttons:
+            pass
+        if Button.UP in buttons:
+            i = 0
+        if Button.DOWN in buttons:
+            break
+
+def sound_abspielen():
+    '''
+    Spielt alle Töne ab
+    '''
+    # Ist zu kompliziert im Moment
+    import inspect 
+    members = inspect.getmembers(SoundFile)
+    members = [(name, data) for (name, data) in members if not name.startswith('_')]
+    i=0
+    while True:
+        name, data = members[i]
+        print('SoundFile.{}'.format(name))
+        brick.display.clear()
+        brick.display.text("")
+        brick.display.text('SoundFile.{}'.format(name))
+        brick.sound.file(data)
+        wait(50)
+        buttons = brick.buttons()
+        while not any(buttons):
+            buttons = brick.buttons()
+            wait(10)
+        if Button.RIGHT in buttons:
+            i += 1
+        if Button.LEFT in buttons: 
+            i -= 1
+        if Button.CENTER in buttons:
+            pass
+        if Button.UP in buttons:
+            i = 0
+        if Button.DOWN in buttons:
+            break
