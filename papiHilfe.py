ab93e60b5ac87b53d0478724a1b26fa52f2d0a0e
Patrick Vogt <patrick.vogt@unibas.ch>
2019-12-31 12:09:56 +0100

    cleanup imports

--- papiHilfe.py
+++ papiHilfe.py
@@ -1,8 +1,6 @@
 from pybricks import ev3brick as brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color,
-                                 SoundFile, ImageFile, Align)
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
