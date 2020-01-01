568f2d108b4069c68dcf8ac0dc5da06d690eebb8
Patrick Vogt <patrick.vogt@unibas.ch>
2020-01-01 13:57:36 +0100

    make main.py save for git

--- .helpers/install.sh
+++ .helpers/install.sh
@@ -1,3 +1,8 @@
-sudo  cp *.py /Users/Andrin/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
+mv main.py main.temp
+mv main_templeate.py main.py
 
+sudo  cp *.py /Users/Andrin/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
 cp *.py /Users/debby/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
+
+mv main.py main_templeate.py 
+mv main.temp main.py 
