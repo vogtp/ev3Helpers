e09ff5568314a3e2b1fd9e8c2be3fb3d1b6ee6fe
Patrick Vogt <patrick.vogt@unibas.ch>
2020-01-02 14:26:48 +0100

    fixes

--- .helpers/install.sh
+++ .helpers/install.sh
@@ -1,10 +1,10 @@
 mv main.py main.temp
-mv main_templeate.py main.py
+mv main_template.py main.py
 
 sudo  cp *.py /Users/Andrin/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
 cp *.py /Users/debby/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
 
-mv main.py main_templeate.py 
+mv main.py main_template.py 
 mv main.temp main.py 
 
 pip3 install pybricks-stubs
