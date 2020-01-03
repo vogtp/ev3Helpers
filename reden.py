c5da3bbc06f9fd115e3d411d3be49ba6ba6b1874
Patrick Vogt <patrick.vogt@unibas.ch>
2020-01-03 13:57:30 +0100

    text to speach

--- reden.py
+++ reden.py
@@ -4,13 +4,55 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile
 from pybricks.tools import print, wait, StopWatch
 import sys 
 import os
+from threading import Thread
+import subprocess
 
-class Reden():
+class Reden(Thread):
+
+    def __init__(self, german=True):
+        self._german = german
+        self._msg = None
+        self.started = False
+        #super().start()
 
     @staticmethod
-    def reden(msg:text, german=True):
-        lang=""
+    def sag( msg, german=True):
         if german:
             lang="-vde"
-        os.system("/usr/bin/espeak -a 200 --stdout '{}' {} > out.wav".format(msg,lang))
+        os.system("/usr/bin/espeak -s 150 -a 200 --stdout '{}' {} > out.wav".format(msg,lang))
         brick.sound.file("out.wav",volume=100)
+
+    def __call__(self, msg):
+        if self.started:
+            self._msg = msg
+        else:
+            Reden.sag(msg, self._german)
+
+
+    def run(self):
+       # self.popen = os.popen("/usr/bin/aplay -i",buffering=1)
+        self.started = True
+        self._espeak("Hallo")
+        try:
+            while 1:
+                if self._msg:
+                    msg = self._msg
+                    self._msg = None
+                    self._espeak(msg)
+                pass
+        except:
+            pass
+        finally:
+            self.started = False
+        
+   
+
+    def _espeak(self, msg):
+        lang=""
+        if self._german:
+            lang="-vde"
+        print(msg)
+        os.system("/usr/bin/espeak -a 200 --stdout '{}' {} | /usr/bin/aplay -i".format(msg,lang))
+        
+      #  brick.sound.file("out.wav",volume=100)
+      #popen.write("/usr/bin/espeak -a 200 --stdout 'test'")
