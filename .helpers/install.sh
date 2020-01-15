1c9f313dba3f76eadf9d1edcc491c19a712677e5
Deborah Vogt <debby@Deborahs-MacBook-Air.local>
2020-01-15 15:54:12 +0100

    fixes

--- .helpers/install.sh
+++ .helpers/install.sh
@@ -3,15 +3,16 @@
 mv main.py main.temp
 mv main_template.py main.py
 
-for USER in Andrin debby
+for USER in Andrin debby lauri
 do
     echo "Installing ${USER} template"
     sudo  rm /Users/${USER}/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/*.py
     sudo  cp *.py /Users/${USER}/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
-    sudo  cp -a .beispiele /${USER}/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
+    sudo  cp -a .beispiele /Users/${USER}/.vscode/extensions/lego-education.ev3-micropython-1.0.3/resources/template/
 done
 
 mv  main.py main_template.py
 mv main.temp main.py 
 
 pip3 install pybricks-stubs --upgrade
+
